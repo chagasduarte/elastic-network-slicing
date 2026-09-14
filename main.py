088eff2a196c00b4351eeb51133d29719ab6090e
chagasduarte <chagasduarte.rdg@gmail.com>
2026-09-14 20:48:59 -0300

    testando as topologias

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from domain.slice_demand import SliceDemand
 from domain.slice_request import SliceRequest
 from pathfinding.dijkstra_path_finder import DijkstraPathFinder
 from services.bandwidth_reservation_service import BandwidthReservationService
+
+#Topologia
 from contracts.create_topo import create_test_graph_fortal
-from domain.node import Node
 from contracts.geant_topology import create_geant_graph
+from contracts.germany50_topology import create_germany50_graph
+
+from domain.node import Node
+
 import random
 
 from algorithms.sa.simulated_annealing_allocator import (
@@ -45,7 +50,7 @@ def main():
     # 1. Criação da topologias
     # ---------------------------------------------------------
 
-    graph = create_geant_graph()
+    graph = create_germany50_graph()
 
     # ---------------------------------------------------------
     # 2. Criação da requisição
